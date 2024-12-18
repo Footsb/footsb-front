770db026d2dd11ad6a4789d76ace7461202b88e3
sstaar91 <sstaar91@gmail.com>
2024-12-18 10:09:24 +0900

    add : 초기 페이지 세팅

---
+++ src/app/match/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Match = () => {
+  return <main className="relative mt-[54px]">123</main>;
+};
+
+export default Match;
---
+++ src/app/team/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Team = () => {
+  return <main className="relative mt-[54px]">123</main>;
+};
+
+export default Team;
