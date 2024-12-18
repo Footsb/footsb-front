1f58e29e5b31eedc82bff188ba8e46b4e3866120
sstaar91 <sstaar91@gmail.com>
2024-12-18 10:02:22 +0900

    add : 초기 세팅

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
-export default {
+const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{html,js}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      oswald: ['oswald'],
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        danger: '#DE3B40',
+        info: '#379AE6',
+        primary: '#4CBB17',
+        disabled: '#565E6C',
+        grayBorder: '#BCC1CA',
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+};
+export default config;
